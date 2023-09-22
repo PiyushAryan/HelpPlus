49033d3a06df19e745fe82e89493d8bd93cb957a
Mohammad Munaf <mohammadmehfooz100@gmail.com>
2023-09-23 02:57:32 +0530

    clearError

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const app = express();
 const path = require('path');
-//const lists = require('./models/list');
+
 const { v4: uuid4 } = require('uuid');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-const Item = require('./models/item');
+const Items = require('./models/item');
 const flash = require('connect-flash');
 
 
@@ -67,14 +67,11 @@ app.post('/register', async (req, res) => {
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
-            res.redirect('/home');
         });
-        //const author = req.user.id;
-        // let allitems = await Items.find({});
-        // res.render('show', { allitems });
+        res.render('home',);
     } catch (e) {
         console.log(e);
-        res.redirect('/register');
+        res.redirect('/home');
     }
 })
 
@@ -83,7 +80,7 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
-    //  let allLists = await lists.find({});
+    //let allLists = await lists.find({});
     res.redirect('/home');
 })
 
@@ -96,91 +93,36 @@ app.get('/logout', (req, res, next) => {
     });
 });
 
-
-
-// app.post('/list/create', isLoggedIn, async (req, res) => {
-//     const { title, text, dueDate } = req.body;
-//     const author = req.user.id;
-//     try {
-//         const list = await lists.create({ title, task: text, author: author, dueDate: dueDate });
-//         console.log('List created:', list);
-//         res.redirect('/list/show');
-//     } catch (error) {
-//         console.error('Error creating list:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-
-app.get('/receive', async (req, res) => {
-    let allItems = await Item.find({});
+app.get('/receive', isLoggedIn, async (req, res) => {
+    const author = req.user.id;
+    let allItems = await Items.find({});
     res.render('receive', { allItems });
 })
 
-app.get('/history', async (req, res) => {
 
+app.get('/donate', isLoggedIn, (req, res) => {
+    res.render('donate');
 })
-// app.post('/list/:id/Status', async (req, res) => {
-//     const id = req.params.id;
-//     const Status = req.body.dueDate;
-//     if (Status) {
-//         const list = await lists.findOne({ _id: id });
-//         list.status = true;
-//         console.log(list);
-//         list.save();
-//     }
-//     res.redirect('/list/show');
-// })
 
-// app.get('/list/create', isLoggedIn, (req, res) => {
-//     res.render('create');
-// })
+app.post('/donate', isLoggedIn, async (req, res) => {
+    const { value, additionalInfo } = req.body;
+    const author = req.user.id;
+    try {
+        const item = await Items.create({ donate: value, des: additionalInfo, author: author });
+        await item.save();
+        console.log('item:', item);
+        res.redirect('/receive');
+    } catch (error) {
+        console.error('Error creating list:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-// app.post('/list/create', isLoggedIn, async (req, res) => {
-//     const { title, text, dueDate } = req.body;
-//     const author = req.user.id;
-//     try {
-//         const list = await lists.create({ title, task: text, author: author, dueDate: dueDate });
-//         console.log('List created:', list);
-//         res.redirect('/list/show');
-//     } catch (error) {
-//         console.error('Error creating list:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-// app.get('/list/edit/:id', isLoggedIn, async (req, res) => {
-//     try {
-//         const list = await lists.findById(req.params.id);
-//         if (list) {
-//             // console.log('List found:', list);
-//             res.render('update', { list });
-//         } else {
-//             console.log('List not found');
-//             res.sendStatus(404);
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//         res.sendStatus(500);
-//     }
-// });
-// app.patch('/list/edit/:id', isLoggedIn, async (req, res) => {
-//     const { id } = req.params;
-//     const newText = req.body.text;
-//     try {
-//         const updatedList = await lists.findByIdAndUpdate(id, { task: newText }, { new: true });
-//         if (updatedList) {
-//             //console.log('List updated:', updatedList);
-//             res.redirect('/list/show');
-//         } else {
-//             console.log('List not found');
-//             res.sendStatus(404);
-//         }
-//     } catch (error) {
-//         console.error('Error updating list:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+app.get('/history/:id',async(req,res)=>{
+    const {id}=req.params;
+    const allItems=await Items.find({author:id});
+    res.render('history',{allItems});
+})
 
 app.listen(3000, () => {
     console.log("connected at 3000");
